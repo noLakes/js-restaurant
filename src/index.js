e1222c5bcfa018ab9f14f6f4f451c9da56762ff4
“noLakes” <51928001+noLakes@users.noreply.github.com>
2021-01-05 20:59:09 -0500

    added content selector and clear function

--- src/index.js
+++ src/index.js
@@ -3,13 +3,18 @@ import setup from './setup';
 console.log('Webpack is working!');
 setup();
 
-const toggleActive = (e) => {
+const content = document.querySelector('.content-container');
+
+function clearContent() {
+  content.innerHTML = '';
+}
+
+function toggleActive(e) {
   if(e.target.classList.contains('active')) return;
   siblings = Array(...e.target.parentNode.children);
   siblings.forEach(el => el.classList.remove('active'));
   e.target.classList.add('active');
 } 
-
 
 /*
 const newElement = (type, className, inner=null) => {
