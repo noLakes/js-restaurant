58cce5ab3b4526f0ea4a83706a868c0ddb919353
“noLakes” <51928001+noLakes@users.noreply.github.com>
2021-01-05 20:30:25 -0500

    added js for nav elements

--- src/setup.js
+++ src/setup.js
@@ -11,6 +11,33 @@ const setup = () => {
   content.classList.add('content-container');
   footer.classList.add('footer-container');
 
+  const nav = document.createElement('ul');
+  nav.classList.add('site-nav');
+
+  const home = document.createElement('li');
+  home.innerHTML = 'Home';
+  home.id = 'home';
+  nav.appendChild(home);
+
+  const menu = document.createElement('li');
+  menu.innerHTML = 'Menu';
+  menu.id = 'menu';
+  nav.appendChild(menu);
+
+  const contact = document.createElement('li');
+  contact.innerHTML = 'Contact';
+  contact.id = 'contact';
+  nav.appendChild(contact);
+
+  header.appendChild(nav);
+
+  const author = document.createElement('a');
+  author.id = 'author';
+  author.innerHTML = '@noLakes';
+  author.href = 'https://github.com/noLakes';
+
+  footer.appendChild(author);
+
   main.appendChild(header);
   main.appendChild(content);
   main.appendChild(footer);
